202ee2454a465df00f4435b168db98d7895a837c
rupam-04 <rpmdey2004@gmail.com>
2024-04-17 15:58:26 +0530

    deploy

--- src/diffie_hellman.rs
+++ src/diffie_hellman.rs
@@ -1,19 +1,20 @@
 use crate::u256::RD256;
 use crate::secp256k1::{Point, SECP256K1};
 
-pub struct DiffieHellman {
+pub struct User {
     pub private_key: RD256,
     pub public_key: Point,
     pub shared_secret: Point
 }
 
-impl DiffieHellman {
+impl User {
     pub fn generate_public_key(&mut self) {
         self.public_key = SECP256K1::mul_point(&SECP256K1::g(), &self.private_key.clone());
     }
 
-    pub fn generate_shared_secret(mut a: DiffieHellman, mut b: DiffieHellman) {
+    pub fn generate_shared_secret(mut a: User, mut b: User) {
         a.shared_secret = SECP256K1::mul_point(&b.public_key.clone(), &a.private_key.clone());
         b.shared_secret = SECP256K1::mul_point(&a.public_key.clone(), &b.private_key.clone());
     }
 }
+
